840ebd95fe2e7bce2f02491f452b62eafff9b1ba
Abdullah Alabbad <abdullah@alabbad.dev>
2023-11-04 16:21:16 +1100

    define UI class

---
+++ UI.cs
@@ -0,0 +1,12 @@
+namespace SocialMediaAnalyser
+{
+    public class UI
+    {
+        public static void Heading(string title, int width = 70)
+        {
+            Console.WriteLine("".PadRight(width, '-'));
+            Console.WriteLine($"> {title}");
+            Console.WriteLine("".PadRight(width, '-'));
+        }
+    }
+}
