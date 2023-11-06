79351bbaf130c63f7ad156c34caf79d4469740d8
Abdullah Alabbad <abdullah@alabbad.dev>
2023-11-06 18:48:48 +1100

    make the app interactive

--- Program.cs
+++ Program.cs
@@ -8,60 +8,70 @@ records.Posts.Add(new Post(10, "Message 1", "username", 123, 321, "12/12/2012 12
 records.Posts.Add(new Post(20, "Message 2", "username", 123, 321, "15/12/2012 12:12"));
 
 // ----------------------------------------------
-// --> Init menu
-// ----------------------------------------------
-string title = "Select from main menu";
-Dictionary<string, string> options =
-    new()
-    {
-        { "1", "Add post" },
-        { "2", "Delete post" },
-        { "3", "Retrieve post by ID" },
-        { "4", "Retrieve the top N posts with most likes" },
-        { "5", "Retrieve the top N posts with most shares" },
-        { "6", "Exist" }
-    };
-
-// ----------------------------------------------
 // --> Run
 // ----------------------------------------------
-string? selectedOption;
+Scene currentScene = Scene.MainMenu;
+int selectedOptionIdx = 0;
+List<string> options =
+    new()
+    {
+        "Add post",
+        "Delete post",
+        "Retrieve post by ID",
+        "Retrieve the top N posts with most likes",
+        "Retrieve the top N posts with most shares",
+        "Exit"
+    };
+
 do
 {
+    Console.Clear();
+
     // Display show menu
-    UI.Heading(title, 70);
-    foreach (string option in options.Keys)
+    if (currentScene == Scene.MainMenu)
     {
-        Console.WriteLine($"{option}) {options[option]}");
+        UI.Heading("Select from main menu", 70);
+        UI.Menu(options, selectedOptionIdx);
+        ConsoleKeyInfo keyInfo = Console.ReadKey();
+        if (keyInfo.Key == ConsoleKey.UpArrow)
+        {
+            selectedOptionIdx = Math.Max(0, selectedOptionIdx - 1);
+        }
+        else if (keyInfo.Key == ConsoleKey.DownArrow)
+        {
+            selectedOptionIdx = Math.Min(options.Count - 1, selectedOptionIdx + 1);
+        }
+        else if (keyInfo.Key == ConsoleKey.Enter)
+        {
+            if (selectedOptionIdx == options.Count - 1)
+            {
+                break;
+            }
+            else if (selectedOptionIdx == 0)
+            {
+                AddPost();
+            }
+            else if (selectedOptionIdx == 1)
+            {
+                DeletePost();
+            }
+            else if (selectedOptionIdx == 2)
+            {
+                GetPostById();
+            }
+            else if (selectedOptionIdx == 3)
+            {
+                GetMostLikedPosts();
+            }
+            else if (selectedOptionIdx == 4)
+            {
+                GetMostSharedPosts();
+            }
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
     }
-    // Get user input
-    selectedOption = Console.ReadLine();
-    Console.WriteLine(selectedOption);
-    switch (selectedOption)
-    {
-        case "1":
-            AddPost();
-            break;
-        case "2":
-            DeletePost();
-            break;
-        case "3":
-            GetPostById();
-            break;
-        case "4":
-            GetMostLikedPosts();
-            break;
-        case "5":
-            GetMostSharedPosts();
-            break;
-        case "6":
-            Console.WriteLine("Exit");
-            break;
-        default:
-            Console.WriteLine("Invalid option");
-            break;
-    }
-} while (selectedOption != "6");
+} while (true);
 
 // ----------------------------------------------
 // --> Functions
@@ -69,11 +79,16 @@ do
 void AddPost()
 {
     Console.WriteLine("TODO: Add post");
+    // TODO: take valid user input
+
+    // TODO: create post obj
+
+    // TODO: add object to the list
 }
 
 void DeletePost()
 {
-    Console.Write("Please enter post ID: ");
+    UI.Prompt("Please enter post ID");
     if (int.TryParse(Console.ReadLine(), out int postId))
     {
         records.DeletePostById(postId);
@@ -105,4 +120,13 @@ void GetMostLikedPosts()
 void GetMostSharedPosts()
 {
     Console.WriteLine("Retrieve the top N posts with most shares");
+}
+
+enum Scene
+{
+    MainMenu,
+    AddPostForm,
+    DeletePostForm,
+    GetPostForm,
+    GetPostsForm
 }
--- UI.cs
+++ UI.cs
@@ -2,8 +2,29 @@ public class UI
 {
     public static void Heading(string title, int width = 70)
     {
+        Console.WriteLine();
         Console.WriteLine("".PadRight(width, '-'));
         Console.WriteLine($"> {title}");
         Console.WriteLine("".PadRight(width, '-'));
+    }
+
+    public static void Menu(List<string> options, int selectedOptionIdx)
+    {
+        for (int i = 0; i < options.Count; i++)
+        {
+            if (i == selectedOptionIdx)
+            {
+                Console.WriteLine($"> {options[i]}");
+            }
+            else
+            {
+                Console.WriteLine($"  {options[i]}");
+            }
+        }
+    }
+
+    public static void Prompt(string prompt)
+    {
+        Console.Write($"{prompt}: ");
     }
 }
