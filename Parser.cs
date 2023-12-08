97d32e38045d44e1c29ae860f144c4992574d3cd
Abdullah Alabbad <abdullah@alabbad.dev>
2023-12-08 17:28:43 +0300

    parse csv add custom messages for exceptions

--- Parser.cs
+++ Parser.cs
@@ -4,12 +4,12 @@ public class Parser
     {
         if (!int.TryParse(value, out int parsedValue))
         {
-            throw new Exception();
+            throw new Exception(message: "Invalid integer");
         }
 
         if (parsedValue < min)
         {
-            throw new Exception();
+            throw new Exception(message: "Integer is out of range");
         }
 
         return parsedValue;
@@ -20,11 +20,11 @@ public class Parser
         string parsedValue = value.Trim();
         if (parsedValue.Length == 0)
         {
-            throw new Exception();
+            throw new Exception(message: "String is empty");
         }
         if (!allowSpaces && parsedValue.Split(" ").Length > 1)
         {
-            throw new Exception();
+            throw new Exception(message: "String contain spaces");
         }
         return parsedValue;
     }
@@ -41,8 +41,22 @@ public class Parser
             )
         )
         {
-            throw new Exception();
+            throw new Exception(message: "Invalid date/time");
         }
         return parsedValue;
+    }
+
+    public static string[] ParseCSV(string str, int expectedFieldsNum)
+    {
+        string[] fields = str.Split(",");
+        if (fields.Length != expectedFieldsNum)
+        {
+            throw new Exception(message: "Invalid number of fields");
+        }
+        for (int i = 0; i < fields.Length; i++)
+        {
+            fields[i] = fields[i].Trim();
+        }
+        return fields;
     }
 }
